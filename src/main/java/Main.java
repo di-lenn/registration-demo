7e06f36617f6152ee088fea3fa6133e18cee53a9
Dylan <dylanfmlee@gmail.com>
2021-11-05 21:39:57 +0000

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import gradleproj1.Course;
+
+public class Main {
+
+    public static void main(String [] args) {
+        //System.out.println("hello");
+
+        Course s = new Course();
+    }
+}
